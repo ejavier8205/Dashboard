01e6ed531a18c589e7a047f73d07392e39db3935
Edward Paz <ejavier8205@gmail.com>
2018-10-28 23:34:15 -0400

    Customer sidebar formating

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "500px";
-    document.getElementById("main").style.marginLeft = "500px";
+    document.getElementById("mySidebar").style.width = "400px";
+    document.getElementById("main").style.marginLeft = "400px";
 }
 
 function closeNav() {
