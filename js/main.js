c4715df79aececddc6338a87070b4c4493ceb5d1
Edward Paz <ejavier8205@gmail.com>
2018-10-28 23:52:56 -0400

    change customers sidebar size

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "400px";
-    document.getElementById("main").style.marginLeft = "400px";
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
 }
 
 function closeNav() {
