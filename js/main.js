f692cd30ce0a72f7826582b8194d4539b06d6fbb
Spartanj117 <44575918+Spartanj117@users.noreply.github.com>
2018-10-31 08:31:56 -0400

    More Buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "300px";
+function openNavAACPS()
+{
+    document.getElementById("AACPS").style.width = "300px";
     document.getElementById("main").style.marginLeft = "300px";
 }
 
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+function closeNavAACPS() 
+{
+    document.getElementById("AACPS").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+function openNavPWCS() 
+{
+    document.getElementById("PWCS").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+}
+
+function closeNavPWCS() 
+{
+    document.getElementById("PWCS").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
 
