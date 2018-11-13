948fddc2d05719cd8256e140ae5e9e9eea50a703
Spartanj117 <44575918+Spartanj117@users.noreply.github.com>
2018-11-13 15:53:28 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -259,3 +259,6 @@ function toggleFullScreen() {
     // After 3 seconds, remove the show class from DIV
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 7000);
 }
+
+/*Clock*/
+
